2f4cf94bda7c901b35a78944559bf3e6e5ec0f1e
Sam Murray <shm31094@gmail.com>
2023-12-18 10:07:00 -0500

    final

---
+++ scripts/main.js
@@ -0,0 +1,27 @@
+fetch("https://randomfox.ca/floof/")
+  .then((response) => response.json())
+  .then((convertedData) => {
+    const foxImgElement = document.querySelector("#fox");
+    foxImgElement.src = convertedData.image;
+  });
+
+
+
+const getTheCat = async () => {
+    const response = await fetch('http://shibe.online/api/cats?count=1&urls=true&httpsUrls=false')
+    const convertedData = await response.json()
+
+    const catImgElement = document.querySelector("#cat")
+    catImgElement.src = convertedData[0]
+
+}
+
+getTheCat()
+
+// fetch("http://shibe.online/api/cats?count=1&urls=true&httpsUrls=false")
+//   .then (response => response.json()) 
+//     .then(convertedData => {
+//     const catImgElement = document.querySelector("#cat");
+//     catImgElement.src = convertedData[0];
+//   }
+// );
